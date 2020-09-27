21e6d6788244c85673cd8cfa8e98f8b273f92211
onema <kinojman@gmail.com>
2020-09-26 22:31:01 -0700

    Update controller to make sure it can be deployed to lambda and paths can be resolved in the java-lambda-container library

--- src/main/kotlin/io/onema/manifestservice/api/StreamController.kt
+++ src/main/kotlin/io/onema/manifestservice/api/StreamController.kt
@@ -14,16 +14,16 @@ package io.onema.manifestservice.api
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import io.onema.manifestservice.config.DynamoDBMapperConfig
 import io.onema.manifestservice.config.OriginConfig
-import io.onema.manifestservice.domain.Segment
-import io.onema.manifestservice.domain.StreamData
 import io.onema.manifestservice.extensions.*
 import io.onema.manifestservice.playlist.buildMasterPlaylist
 import io.onema.manifestservice.playlist.buildMediaPlaylist
+import io.onema.manifestservice.playlist.master
+import io.onema.manifestservice.service.FileService
 import org.apache.commons.vfs2.FileObject
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Import
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -36,39 +36,56 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 @RestController
 @EnableWebMvc
 @Import(value = [OriginConfig::class, DynamoDBMapperConfig::class])
-class StreamController(private val files: List<FileObject>, private val mapper: DynamoDBMapper) {
+class StreamController(
+    private val dir: List<FileObject>,
+    private val fileService: FileService,
+    private val mapper: DynamoDBMapper) {
 
+    //--- Fields ---
     private val log = LoggerFactory.getLogger(javaClass)
-    private val renditionMetadata: Map<String, StreamData> = files.renditionMetadata()
-    private val renditionSegments: Map<String, List<Segment>> = files.renditionSegments(renditionMetadata)
     private val mimeTypeM3U8: String = "application/x-mpegURL"
     private val mimeTypeTS: String = "video/MP2T"
 
-    @GetMapping("/master.m3u8")
-    fun masterM3U8(): ResponseEntity<String> {
-        log.info("/master.m3u8")
+    //--- Methods ---
+    @GetMapping("/video/{videoName}/master")
+    fun masterM3U8(@PathVariable videoName: String): ResponseEntity<String> {
 
-        val body = buildMasterPlaylist(renditionMetadata)
+        log.info("/video/$videoName/master")
+        val renditionMetadata = mapper.renditionMetadata(videoName)
+        val body = buildMasterPlaylist(videoName, renditionMetadata)
+        master playlist {
+            version value 5
+        }
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, mimeTypeM3U8).body(body)
     }
 
-    @GetMapping("/media/{mediaName}")
-    fun mediaHandler(@PathVariable mediaName: String): ResponseEntity<String> {
-        log.info("/media/$mediaName.m3u8")
+    @GetMapping("/video/{videoName}/media/{mediaName}")
+    fun mediaHandler(@PathVariable videoName: String, @PathVariable mediaName: String): ResponseEntity<String> {
 
-        val segments = renditionSegments[mediaName]
-            ?: throw ResponseStatusException(NOT_FOUND, "Invalid rendition ID $mediaName")
-        val body = buildMediaPlaylist(segments, mediaName)
+        log.info("/video/$videoName/media/$mediaName")
+        val renditionSegments = mapper.renditionSegments(videoName, mediaName)
+        val body = buildMediaPlaylist(videoName, renditionSegments, mediaName)
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, mimeTypeM3U8).body(body)
     }
 
-    @GetMapping("/segment/{segmentName}")
-    fun segmentHandler(@PathVariable segmentName: String, @RequestHeader("range") range: String): ResponseEntity<ByteArray> {
-        log.info("/segment/$segmentName.ts")
+    @GetMapping("/video/{videoName}/segment/{segmentName}")
+    fun segmentHandler(
+        @PathVariable videoName: String,
+        @PathVariable segmentName: String,
+        @RequestHeader("range") range: String
+    ): ResponseEntity<ByteArray> {
+
+        log.info("/video/$videoName/segment/$segmentName $range")
         val (start, end) = range.splitRange()
+        val files: List<FileObject> = dir.first { it.nameWithoutExtension == videoName }.children.toList()
         val file = files.findVideoByRenditionId(segmentName)
-            ?: throw ResponseStatusException(NOT_FOUND, "Segment $segmentName not found")
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Segment $segmentName not found")
+        val segment = fileService.readBytes(file, start, end)
 
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, mimeTypeTS).body(file.segment(start, end))
+        log.info("Sending back ${segment.size} bytes")
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_TYPE, mimeTypeTS)
+            .header("Keep-Alive", "timeout=60")
+            .body(segment)
     }
 }
